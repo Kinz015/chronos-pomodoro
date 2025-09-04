d59baf4b92a57358a3f024fe61b26e9cd440aa58
Guilherme Lins <guilhermelins978@gmail.com>
2025-09-03 23:20:49 -0300

    save

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,17 +3,60 @@ import { Cycles } from "../Cycles";
 import { DefaltButton } from "../DefaultButton";
 import { DefaltInput } from "../DefaultInput";
 import styles from "./styles.module.css";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // Conferir
+        secondsRemaining, // Conferir
+        formattedSecondsRemaining: "00:00", // Conferir
+        tasks: [ ...prevState.tasks, newTask ]
+      };
+    });
+  }
 
   return (
-    <form className={styles.form} action="">
+    <form onSubmit={handleCreateNewTask} className={styles.form} action="">
       <div className={styles.formRow}>
         <DefaltInput
           type="text"
           id="input"
           labelText={"task"}
           placeholder="Digite algo"
+          ref={taskNameInput}
         ></DefaltInput>
       </div>
 
